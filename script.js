350fde1004369d06a840ea2f9be6341dd38284da
Daniel Gil <t-dagil@microsoft.com>
2017-03-29 20:40:40 -0400

    Random keys are activated every 500ms. User key press deactivates them

--- script.js
+++ script.js
@@ -1,36 +1,82 @@
-var keyCodes = {
+var keyCodeToChar = {
 	65 : "a",
 	83 : "s",
 	68 : "d",
 	70 : "f",
-	71 : "g",
-	72 : "h",
+	//71 : "g",
+	//72 : "h",
 	74 : "j",
 	75 : "k",
 	76 : "l",
 	186 : ";"
 };
 
-function activateKey(code) {
-	if (code in keyCodes) {
-		var key = document.getElementById(keyCodes[code]);
-		key.style.background = "#FF9999";
+var activated = {
+	65 : false,
+	83 : false,
+	68 : false,
+	70 : false,
+	//71 : "g",
+	//72 : "h",
+	74 : false,
+	75 : false,
+	76 : false,
+	186 : false
+};
+
+var keyCodes = [65, 83, 68, 70, 74, 75, 76, 186];
+
+
+function start() {
+	// calls update every half second
+	setInterval(update, 500);
+}
+
+function update() {
+	randomKey();
+}
+
+// randomly activates a key
+function randomKey() {
+	var keyCode = keyCodes[Math.floor(Math.random() * keyCodes.length)];
+	if (!activated[keyCode]) {
+		activateKey(keyCode);
 	}
 }
 
+// changes a key to specified color
+function changeColor(key, color) {
+	key.style.background = color;
+}
+
+// changes a key to color red
+function activateKey(code) {
+	if (code in keyCodeToChar) {
+		var key = document.getElementById(keyCodeToChar[code]);
+		changeColor(key, "#FF9999");
+		activated[code] = true;
+	}
+}
+
+// changes a key back to color grey
 function deactivateKey(code) {
-	if (code in keyCodes) {
-		var key = document.getElementById(keyCodes[code]);
-		key.style.background = "#EFF0F2";
+	if (code in keyCodeToChar) {
+		var key = document.getElementById(keyCodeToChar[code]);
+		changeColor(key, "#EFF0F2");
+		activated[code] = false;
 	}
 }
 
 // key unpressed
 window.onkeyup = function(e) {
-	deactivateKey(e.keyCode);
+	//deactivateKey(e.keyCode);
 }
 
 // key pressed
 window.onkeydown = function(e) {
-	activateKey(e.keyCode);
+	//activateKey(e.keyCode);
+	deactivateKey(e.keyCode);
 }
+
+// starts the timer
+start();
